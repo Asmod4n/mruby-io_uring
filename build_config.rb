65b7ce20f83cf656fd94bcd9d6372682f37e3077
Asmod4n <791770+Asmod4n@users.noreply.github.com>
2025-10-05 15:56:12 +0200

    update liburing to 2.12

--- build_config.rb
+++ build_config.rb
@@ -1,8 +1,8 @@
 MRuby::Build.new do |conf|
     toolchain :gcc
-    #conf.enable_sanitizer "address,undefined,leak"
+    conf.enable_sanitizer "address,undefined,leak"
     conf.cc.flags << '-fno-omit-frame-pointer' << '-g' << '-ggdb' << '-Og'
-    #conf.linker.flags_before_libraries << '-static-libasan'
+    conf.linker.flags_before_libraries << '-static-libasan'
     #conf.cc.flags << '-Os' << '-march=native'
     conf.enable_debug
     conf.enable_test
@@ -10,7 +10,6 @@ MRuby::Build.new do |conf|
     conf.gem File.expand_path(File.dirname(__FILE__))
     conf.gem mgem: 'mruby-phr'
     conf.gem mgem: 'mruby-chrono'
-    conf.gem mgem: 'mruby-signal'
     conf.gem :core => 'mruby-bin-debugger'
     conf.gem :core => 'mruby-io'
     conf.gem :core => 'mruby-socket'
